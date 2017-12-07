761071f01aab30b79e6d4a6982acc6ca8a110f3f
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2017-12-06 19:11:58 -0800

    Javascript begun
    gamestart function works
    sets guesses remaining
    inputs corrrect number of blanks
    accepts a letter as verified by console.log
    isLetterInWord function also running

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,51 @@
-// Array of words for game to chose from (an array of arrays)
+/// Array of words for game to chose from (an array of arrays)
 
-var wordLibrary = [ ["t","h","i","s"], ["t","h","a","t"], ["t","h","o","s","e"]];
+var wordLibrary = [ "this", "that", "those", "sometimes", "never", "always", "buffoon", "Pennywise" ];
+var alreadyGuessed = []
 
-// Randomly selects one of the indexes from the above array
-var x = Math.floor(Math.random() * wordLibrary.length);
+function gameStart() {
+    // Establishes that the word has not been completed by guesses, will check against in later functions
+    wordFinished = false;
+    // currentWord to play gets assigned a random index from wordLibrary
+    var currentWord = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];
+    // sets starting guess limit to 6
+    var guessesRemaining = 6
+    // enters guess guessesRemaining value to <div id = #guesses-remaining>
+    document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
+    // draws the blanks...
+    document.getElementById("current-word").innerHTML = "_".repeat(currentWord.length);
+    
+    console.log(currentWord);
+    console.log(currentWord.length);
+    
+};
 
-// Assigns array ar index[x] or wordLibrary as current word playing
-var currentWord = wordLibrary[x];
+function isLetterInWord(letter) {
+    if (guessesRemaining > 0 && wordFinished != true) {
+        if (currentWord.indexOf(letter) > -1) {
+            correctGuess(letter);
+        }
+        else {
+            incorrectGuess(letter);
+        }
+    }
+}
 
-var currentWordLength = currentWord.length
 
-console.log(x);
-console.log(currentWord);
-console.log(currentWordLength)
+
+    // for (var i = 0; i < currentWord.length; i++){
+
+    // }
+
+
+
+
+
+gameStart();
+document.onkeyup = function(event) {    
+    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    isLetterInWord
+    console.log(letter)
+}
+
+// var letter = String.fromCharCode(event.keyCode).toLowerCase();
