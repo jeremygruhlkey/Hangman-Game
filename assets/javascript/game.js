1216a8cc71f92a32ec7335cf15a73249bd7dff48
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2017-12-06 20:49:20 -0800

    Added correctGuess() function
    it doesn't work...
    mainly b/c currentWord.indexOf(letter) ALWAYS returns -1
    git commit - m

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,18 @@
-/// Array of words for game to chose from (an array of arrays)
-
+// Array of words for game to chose from (an array of arrays)
 var wordLibrary = [ "this", "that", "those", "sometimes", "never", "always", "buffoon", "Pennywise" ];
+// An empty array to push incorrect guesses to
 var alreadyGuessed = []
+// sets starting guess limit to 6
+var guessesRemaining = 6
+// declares currentWord variable
+var currentWord=""
 
 function gameStart() {
     // Establishes that the word has not been completed by guesses, will check against in later functions
     wordFinished = false;
     // currentWord to play gets assigned a random index from wordLibrary
     var currentWord = wordLibrary[Math.floor(Math.random() * wordLibrary.length)];
-    // sets starting guess limit to 6
-    var guessesRemaining = 6
+    
     // enters guess guessesRemaining value to <div id = #guesses-remaining>
     document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
     // draws the blanks...
@@ -18,18 +21,36 @@ function gameStart() {
     console.log(currentWord);
     console.log(currentWord.length);
     
+    
+    
+    
 };
 
+// function checks if letter is in currentWord...
 function isLetterInWord(letter) {
     if (guessesRemaining > 0 && wordFinished != true) {
         if (currentWord.indexOf(letter) > -1) {
-            correctGuess(letter);
+            function correctGuess(letter) {
+                for (var i = 0; i < currentWord.length; i++){
+                    if (letter == currentWord[i]) {
+                        var old = document.getElementById("current-word").innerHTML;
+                        var replace = old.replace(currentWord[i], letter);
+                        document.getElementById("current-word").innerHTML = replace;
+                    }
+                    if (document.getElementById("current-word").innerHTML.indexOf("_") < 0) {
+                        alert("you win!");
+                        wordFinished = true;
+                    }
+                }
+            }
         }
         else {
-            incorrectGuess(letter);
+            function incorrectGuess(letter) {
+
+            }
         }
     }
-}
+  };  
 
 
 
@@ -44,8 +65,11 @@ function isLetterInWord(letter) {
 gameStart();
 document.onkeyup = function(event) {    
     var letter = String.fromCharCode(event.keyCode).toLowerCase();
-    isLetterInWord
-    console.log(letter)
-}
+    isLetterInWord(letter);
+    
+    console.log(letter);
+    var letterPosition = currentWord.indexOf(letter);
+    console.log(letterPosition);
+};
 
 // var letter = String.fromCharCode(event.keyCode).toLowerCase();
